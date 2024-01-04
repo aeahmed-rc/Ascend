f8ace2ad61d40bfeb9f730301f1890528c6a6258
aeahmed-rc <a.ahmed9316@gmail.com>
2024-01-04 16:25:11 -0500

    ex 1.6

--- FullStack/Part0/Part1/part1/src/App.jsx
+++ FullStack/Part0/Part1/part1/src/App.jsx
@@ -1,20 +1,36 @@
-const Hello =(props)=>{
-  console.log(props)
-  return(
-    <div>
-      <p>Hello {props.name} you are {props.age} years old</p>
-    </div>
-  )
-}
+import { useState } from 'react'
+import Button from './Button'
 
 const App = () => {
-  const age=10  // variable with value will be sent as a prop from parent to child component
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const Handlegood=()=>{
 
+    setGood(good+1)
+    console.log(good)
+  }
+  const Handlebad=()=>{
+
+    setBad(bad+1)
+    console.log(bad)
+  }
+  const Handleneutral=()=>{
+ 
+    setNeutral(neutral+1)
+    console.log(neutral)
+  }
   return (
     <div>
-      <p>Greetings</p>
-      <Hello name='George' age={age}/>
-      <Hello name ='Daisy'/>
+      <h2>Give Feedback</h2>
+      <Button onClick={Handlegood} text="good"/>
+      <Button onClick={Handlebad} text="bad"/>
+      <Button onClick={Handleneutral} text="neutral"/>
+      <h2>Statisitcs</h2>
+      <h3>good:{good}</h3>
+      <h3>bad:{bad}</h3>
+      <h3>neutral:{neutral}</h3>
     </div>
   )
 }
